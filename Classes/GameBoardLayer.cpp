6341b3239d392ff649088403990a4a3a66653f74
MonsterMasterAF <andrewbf@lenovo.com>
2017-02-27 12:51:43 +0800

    remove unuse code

--- Classes/GameBoardLayer.cpp
+++ Classes/GameBoardLayer.cpp
@@ -22,73 +22,12 @@ GameBoardLayer::~GameBoardLayer()
 bool GameBoardLayer::init()
 {
     m_listener = EventListenerTouchOneByOne::create();
-    //PlayScene* controller;
-    //controller = m_controller;
-    //std::vector<BlockView*> blockView;
-    //blockView = blocks;
-    /*
-    m_listener->onTouchBegan = [](Touch* touch, Event* event)
-    {
-        return true;
-    };
-    
-    m_listener->onTouchMoved = [blockView, controller](Touch* touch, Event* event)
-    {
-        XXL_Position pos;
-        for(auto& item : blockView)
-        {
-            if(item->getTextureRect().containsPoint(touch->getLocation()))
-            {
-                auto xMovedLength = touch->getLocation().x-touch->getStartLocation().x;
-                auto yMovedLength = touch->getLocation().y-touch->getStartLocation().y;
-                MoveBy* moveBy;
-                if((std::abs(xMovedLength) > std::abs(yMovedLength)))
-                {
-                    pos.xDirection = xMovedLength;
-                    if(xMovedLength > 0)
-                    {
-                        moveBy = MoveBy::create(0.5, Vec2(0, item->getContentSize().width));
-                    }
-                    else
-                    {
-                        moveBy = MoveBy::create(0.5, Vec2(0, -item->getContentSize().width));
-                    }
-                }
-                else
-                {
-                    pos.yDirection = yMovedLength;
-                    if(yMovedLength > 0)
-                    {
-                        moveBy = MoveBy::create(0.5, Vec2(item->getContentSize().height,0));
-                    }
-                    else
-                    {
-                        moveBy = MoveBy::create(0.5, Vec2(-item->getContentSize().height, 0));
-                    }
-                }
-                item->runAction(moveBy);
-                pos.x = (item->getPositionX())/(item->getContentSize().width);
-                pos.y = (item->getPositionY()-(Director::getInstance()->getVisibleSize().height/2-item->getContentSize().height*3))/(item->getContentSize().height);
-            }
-        }
-        
-        
-        controller->selectBlock(pos);
-        return true;
-    };
-    
-    m_listener->onTouchEnded = [](Touch* touch, Event* event)
-    {
-        return true;
-    };
-    */
     return true;
 }
 
 bool GameBoardLayer::initWithBlockModels(std::vector<std::vector<Block*>> blockMetrics)
 {
     //TODO: draw the matrixs according to the model data
-    //Vec2 startPoint = Vec2(bXCol*this->getContentSize().width, bYRow*this->getContentSize().height+Director::getInstance()->getVisibleSize().height/2-this->getContentSize().height*3);
     float ySize = blockMetrics.size();
     float xSize = (*blockMetrics.begin()).size();
     std::vector<std::vector<Block*>>::iterator rows;
@@ -128,24 +67,6 @@ bool GameBoardLayer::initWithBlockModels(std::vector<std::vector<Block*>> blockM
     
     m_listener->onTouchBegan = [blocks](Touch* touch, Event* event)
     {
-        
-        for(auto& item : blocks)
-        {
-            Point posi = Director::getInstance()->convertToGL(touch->getLocationInView());
-            int x = posi.x;
-            int y = posi.y;
-            Rect MinX = item->getTextureRect();
-            Rect MaxX = item->getTextureRect();
-            //Rect::containsPoint(item->getBoundingBox(), )
-            Rect a = item->getBoundingBox();
-            Size b = item->getContentSize();
-            Rect c = item->getTextureRect();
-            if(item->getBoundingBox().containsPoint(Director::getInstance()->convertToGL(touch->getLocationInView())))
-            {
-                return true;
-            }
-        }
-        
         return true;
     };
     
